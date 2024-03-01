d96e529995be66607708ea06310ced9b868687a9
kwoneggrice <kwjddls1919@gmail.com>
2024-03-01 20:51:20 +0900

    Feat: 쿼리 인터페이스 구현

---
+++ UserManagement/IServices/IQuery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagement.IServices
+{
+	public interface IQuery<T>
+	{
+		// 전체 데이터 조회
+		List<T> GetAll();
+
+		// 특정 데이터 조회
+		T GetOne(int id);
+
+		// 데이터 추가
+		void Create(T entity);
+
+		// 데이터 수정
+		void Update(T entity);
+
+		// 데이터 삭제
+		void Delete(int id);
+	}
+}
