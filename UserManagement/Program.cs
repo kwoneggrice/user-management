be4d7122f5bac3307c2ab5dbc5c110a60edba0df
kwoneggrice <kwjddls1919@gmail.com>
2024-03-01 22:23:56 +0900

    Feat: DI, DBContext 적용

--- UserManagement/Program.cs
+++ UserManagement/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.Extensions.DependencyInjection;
+using UserManagement.Forms;
+using UserManagement.IServices;
+using UserManagement.Models;
+using UserManagement.Services;
+
 namespace UserManagement
 {
 	internal static class Program
@@ -11,7 +17,28 @@ namespace UserManagement
 			// To customize application configuration such as set high DPI settings or default font,
 			// see https://aka.ms/applicationconfiguration.
 			ApplicationConfiguration.Initialize();
-			Application.Run(new Form1());
+
+			// 서비스 컨테이너 생성
+			var services = new ServiceCollection();
+
+			// 데이터베이스와 상호작용 할 Context 등록
+			services.AddDbContext<UserManagementContext>();
+
+			// 싱글턴 패턴 적용
+			services.AddSingleton<DashBoard>();
+
+			// AddTransient 메소드를 사용해 필요할 때 객체 생성하고 반환
+			services.AddTransient<IQuery<User>, UserService>();
+			services.AddTransient<UserCreate>();
+			services.AddTransient<UserUpdate>();
+
+			// 서비스 컨테이너에 등록된 서비스들을 생성하고 관리하는 ServiceProvider
+			using ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+			// DashBoard 클래스 인스턴스 요청
+			var dashBoard = serviceProvider.GetRequiredService<DashBoard>();
+
+			Application.Run(dashBoard);
 		}
 	}
 }
