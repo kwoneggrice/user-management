f8f5c45e5c59698874a8c94c8b5302a68cca41f8
kwoneggrice <kwjddls1919@gmail.com>
2024-03-05 19:42:14 +0900

    Feat: 패스워드 단방향 암호화 구현

---
+++ UserManagement/Utils/PasswordUtil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagement.Utils
+{
+	public class PasswordUtil
+	{
+		public static string HashingPassword(string password)
+		{
+			using (SHA256 hash = SHA256.Create())
+			{
+				byte[] bytes = hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+				StringBuilder sb = new StringBuilder();
+
+				for (int i = 0; i < bytes.Length; i++)
+				{
+					sb.Append(bytes[i].ToString("x2"));
+				}
+
+				return sb.ToString();
+			}
+		}
+	}
+}
